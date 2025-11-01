aa254d29b4534d9f3ed11affd81e16dc2453dafb
hayyan <hayyan.gov@gmail.com>
2025-11-01 23:15:45 +0400

    good

---
+++ android/app/src/main/java/com/azkaar/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.azkaar.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.azkaar.app',
+  appName: 'Azkaar',
+  webDir: 'dist/azkaar/browser'
+};
+
+export default config;
