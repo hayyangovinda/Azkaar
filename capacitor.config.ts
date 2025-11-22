a88dcc5593200e56af3c1a3ab05c80b9dc27d502
hayyan <hayyan.gov@gmail.com>
2025-11-22 07:59:03 +0400

    goood

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.azkaar.app',
   appName: 'Azkaar',
-  webDir: 'dist/azkaar/browser'
+  webDir: 'dist/azkaar/browser',
 };
 
 export default config;
