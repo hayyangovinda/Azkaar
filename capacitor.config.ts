6cd4cf21a2cf3d37e9a15ecee1c236b8c5508326
hayyan <hayyan.gov@gmail.com>
2025-11-22 09:43:38 +0400

    fix back butn

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,10 +4,6 @@ const config: CapacitorConfig = {
   appId: 'com.azkaar.app',
   appName: 'Azkaar',
   webDir: 'dist/azkaar/browser',
-  server: {
-    url: 'http://172.20.10.6:4200',
-    cleartext: true
-  }
 };
 
 export default config;
